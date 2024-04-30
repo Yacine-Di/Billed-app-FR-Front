116e43bfbf96eb5d869931abd0d62c8c18994653
Yacine Diaf <yacinediafwork@gmail.com>
2024-04-30 19:58:07 +0200

    ajout de test dans Bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,16 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import { fireEvent, screen, wait, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
+import Bills from "../containers/Bills.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import store from "../__mocks__/store.js"
 
 import router from "../app/Router.js";
+import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -18,6 +21,7 @@ describe("Given I am connected as an employee", () => {
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
+
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
@@ -28,13 +32,87 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
       expect(windowIcon.classList.contains("active-icon")).toBeTruthy()
     })
-    
-    /*test("Then bills should be ordered from earliest to latest", () => {
+
+    test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
-    })*/
+    })
+
+    test("handleClickIconEye is called when the eye icon is clicked", () => {
+      const billsInstance = new Bills({ document, onNavigate: jest.fn(), store })
+      const mockIcon = document.createElement("div")
+      mockIcon.setAttribute("data-bill-url", "mockBillUrl")
+
+      billsInstance.handleClickIconEye = jest.fn()
+
+      mockIcon.addEventListener("click", () => billsInstance.handleClickIconEye(mockIcon))
+
+      mockIcon.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+
+      expect(billsInstance.handleClickIconEye).toHaveBeenCalledWith(mockIcon)
+    })
+
+    test("Should get bills from store with getbills methode", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      document.body.innerHTML = BillsUI({ data: bills })
+
+      const billsInstance = new Bills({ document, onNavigate, store, localStorage })
+      const billsFromMethode = await billsInstance.getBills()
+
+      expect(billsFromMethode.length).toEqual(4)
+    })
+
+    test("Should get bills error from getbills methode", async () => {
+      const mockError = new Error("Fake error")
+      const wrongDataStore = {
+        bills: jest.fn(() => ({
+          list: jest.fn(() => Promise.reject(mockError))
+        }))
+      }
+      const billsInstance = new Bills({ document, onNavigate, store: wrongDataStore })
+
+      try {
+        await billsInstance.getBills()
+      } catch (err) {
+        expect(err).toBe(mockError)
+      }
+    })
+
+    test("Should handle click on eye icon", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      document.body.innerHTML = BillsUI({ data: bills })
+      const billsInstance = new Bills({ document, onNavigate, store, localStorage })
+      //mock handle function
+      const handleClickIconEye = jest.fn(billsInstance.handleClickIconEye)
+      const icons = screen.getAllByTestId("icon-eye")
+      //mock modal function
+      window.$.fn.modal = jest.fn()
+      icons.forEach(icon => {
+        icon.addEventListener("click", handleClickIconEye(icon))
+      })
+
+      //check if calls work for one icon
+      userEvent.click(icons[0])
+      expect(handleClickIconEye).toHaveBeenCalled()
+
+      const modale = screen.getByTestId("modaleFile")
+      expect(modale).toBeTruthy()
+    })
   })
 })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,9 +5,9 @@
 import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-import { ROUTES, ROUTES_PATH } from "../constants/routes"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
 import { localStorageMock } from "../__mocks__/localStorage.js"
-import mockStore from "../__mocks__/store"
+import mockStore from "../__mocks__/store.js"
 import userEvent from "@testing-library/user-event"
 
 beforeEach(() => {
@@ -20,16 +20,37 @@ afterEach(() =>  {
 })
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then new bill should submited", () => {
+  describe("When I am on NewBill Page and form is filled correctly", () => {
+    test("Then new bill should be submited", () => {
       //to-do write assertion
+      const inputDepenseName = screen.getByTestId("expense-name")
+      expect(inputDepenseName.value).toBe("")
+
+      const inputDate = screen.getByTestId("datepicker")
+      fireEvent.change(inputDate, {target: {value: "2024-04-28"}})
+      expect(inputDate.value).toBe("2024-04-28")
+
+      const inputAmount = screen.getByTestId("amount")
+      fireEvent.change(inputAmount, { target: {value: 250}})
+      expect(Number(inputAmount.value)).toBe(250)
+
+      const inputTVA = screen.getByTestId("vat")
+      fireEvent.change(inputTVA, { target: {value: 10}})
+      expect(Number(inputTVA.value)).toBe(10)
+
+      const inputPercentage = screen.getByTestId("pct")
+      fireEvent.change(inputPercentage, { target: {value: 5}})
+      expect(Number(inputPercentage.value)).toBe(5)
+
+      const inputCommentary = screen.getByTestId("commentary")
+      fireEvent.change(inputCommentary, { target: {value: "Ceci est un commentaire"}})
+      expect(inputCommentary.value).toBe("Ceci est un commentaire")
 
       const form = screen.getByTestId("form-new-bill")
-      
-      const inputDepenseName = screen.getByTestId("expense-name")
-      fireEvent.change(inputDepenseName, {target: {value: "Vol et train"}})
-      expect(inputDepenseName.value).toBe("Vol et train")
-
+      const handleSubmit = jest.fn((e) => e.preventDefault())
+      form.addEventListener("submit", handleSubmit)
+      fireEvent.submit(form)
+      expect(handleSubmit).toHaveBeenCalled()
     })
   })
 })
